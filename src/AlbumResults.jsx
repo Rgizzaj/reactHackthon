098f206688329b269def7bedcdfbe011641ddd11
Kurt Rode <kurtrode@seznam.cz>
2023-03-10 15:49:19 +0100

    last minute changes

--- src/AlbumResults.jsx
+++ src/AlbumResults.jsx
@@ -7,9 +7,9 @@ export default function AlbumResults(props){
              props.artist.map((artists,index)=>{
                 return(
                 <div key={index}>
-    <h3>{artists.title}</h3>
-                <p>{artists.country}</p>
-                <p>{artists.date}</p> 
+    <h3>Album: {artists.title}</h3>
+                <p>Released in: {artists.country}</p>
+                <p>Release Date: {artists.date}</p> 
                 </div>)
              })
                 
